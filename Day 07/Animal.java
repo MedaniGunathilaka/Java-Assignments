4a40f2bc078ac4023c688e6994be5e7af53a93dd
MedaniGunathilaka <hansikamedani@gmail.com>
2020-12-16 08:00:48 +0530

    Added a java file

---
+++ Day 07/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    private String eats;
+    private int noOfLegs;
+
+    public Animal(){
+
+    }
+    public Animal(String food,int legs){
+        this.eats=food;
+        this.noOfLegs=legs;
+    }
+    public String getEats(){
+        return eats;
+    }
+    public void setEats(String eats){
+        this.eats=eats;
+    }
+    public int getNoOfLegs(){
+        return noOfLegs;
+    }
+    public void setNoOfLegs(int noOfLegs){
+        this.noOfLegs=noOfLegs;
+    }
+
+}
