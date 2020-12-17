0def594b3a9e72e53659846624b86f28650e8dfe
MedaniGunathilaka <hansikamedani@gmail.com>
2020-12-17 16:25:14 +0530

    Added a java file

---
+++ Day 11/SimpleArithmeticCalculator.java
@@ -0,0 +1,34 @@
+
+
+package arithmeticCalculator;
+
+ public class SimpleArithmeticCalculator{
+	 
+ public void add(int num1, int num2){
+	 
+	 int answer=num1+num2;
+	 System.out.println("Addition = " +answer);
+ }
+ 
+  public void substract(int num1, int num2){
+	 
+	 int answer=num1-num2;
+  System.out.println("Substract= " +answer);
+  
+  }
+	  public void division(int num1, int num2){
+   
+	 
+	  int answer=num1/num2;
+	  System.out.println("Division = " +answer);
+	  
+	  }
+	  
+	  public void multiplication(int num1, int num2){
+	 
+	 int answer=num1*num2;
+	 System.out.println("Multiplication = " +answer);
+ 
+ }
+ }
+ 
