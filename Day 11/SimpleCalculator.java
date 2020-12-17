18204d8131368f1f2334721c7b67ce7166a4d9f3
MedaniGunathilaka <hansikamedani@gmail.com>
2020-12-17 16:24:21 +0530

    Added a java file

---
+++ Day 11/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+ public class SimpleCalculator{
+ public int interestCalculation(int payment, int rate){
+	 
+	 int interestAmount=payment*rate/100;
+	 return interestAmount;
+ }
+ }
+ 
